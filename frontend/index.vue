9a6e5a573278fa0bbfe586017a42848fb6cf3d5a
GitVerse <keencode_eth@163.com>
2023-02-06 23:39:32 +0800

    add gv page

---
+++ frontend/index.vue
@@ -0,0 +1,18 @@
+<route lang="yaml">
+meta:
+  layout: gitverse
+</route>
+
+<script setup type="ts">
+const { toggleShowNewForm, items, isListLoading } = $(gvStore())
+</script>
+
+<template>
+  <div>
+    <Loading v-if="isListLoading" class=" p-40" />
+    <Empty v-else-if="items.length === 0" class="my-20" @newBtnClick="toggleShowNewForm">
+      New Project
+    </Empty>
+    <GvItems v-else :items="items" />
+  </div>
+</template>
